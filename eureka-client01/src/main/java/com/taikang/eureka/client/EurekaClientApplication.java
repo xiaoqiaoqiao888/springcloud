13331fdd568400dcf35b92821ce016155e8fadbb
xiaoqiaoqiao888 <qiaodongjie666@163.com>
2018-06-04 10:00:31 +0800

    增加eureka-server client

---
+++ eureka-client01/src/main/java/com/taikang/eureka/client/EurekaClientApplication.java
@@ -0,0 +1,26 @@
+package com.taikang.eureka.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "hi " + name + ",I come from port:" + port;
+    }
+}
---
+++ eureka-server/src/main/java/com/taikang/eureka/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.taikang.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 
+ * @author qiaodj
+ * @date 2017年7月13日
+ * @version 1.0
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
